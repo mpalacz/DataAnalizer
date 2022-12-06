14db4cd9bd4c26090ee201f5fd75896555c869ad
mpala <mpalacz00@gmail.com>
2022-12-06 22:22:35 +0100

    Admin view

--- api/DataHandler/StockDataManager/admin.py
+++ api/DataHandler/StockDataManager/admin.py
@@ -1,2 +1,20 @@
 from django.contrib import admin
 
+from .models import Stock, Analysis, DataTable
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Analysis)
+class AnalysisAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DataTable)
+class DataTableAdmin(admin.ModelAdmin):
+    pass
+
+
